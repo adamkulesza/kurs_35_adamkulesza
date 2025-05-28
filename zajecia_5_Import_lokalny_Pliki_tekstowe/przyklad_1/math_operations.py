175344320c18917ce673082a3547cac7453e4770
adamkulesza <adamkulesza>
2025-05-28 10:26:16 +0200

    zmiana nazw folderow

---
+++ zajecia_5_Import_lokalny_Pliki_tekstowe/przyklad_1/math_operations.py
@@ -0,0 +1,13 @@
+#tworzymy dwie funkcje
+#dodawanie
+"""Funkcja dodaje dwie liczby"""
+def dodawanie(liczba_1, liczba_2):
+    return liczba_1 + liczba_2
+
+#odejmowanie
+def odejmowanie(liczba_1, liczba_2):
+    return liczba_1 - liczba_2
+
+#aby wywolac ta funkcje robimy
+
+print(dodawanie(1, 2))
